781ca63a29937871a303f57abeaf8537758257fa
jan <jan@martins-mini.fritz.box>
2017-08-19 22:38:39 +0200

    Start Weather Server Project

---
+++ WeatherServer/src/de/martin70m/weather/server/WeatherServer.java
@@ -0,0 +1,10 @@
+package de.martin70m.weather.server;
+
+public class WeatherServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Server starts...");
+	}
+
+}
